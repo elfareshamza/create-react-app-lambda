634d360bdd9c167ad329167c10b8d8f2ef00fe14
“elfareshamza” <elfareshamza@gmail.com>
2023-03-30 09:57:37 +0000

    fix url android and ios redirect

--- src/App.js
+++ src/App.js
@@ -6,8 +6,8 @@ import "./App.css"
 const LambdaDemo = () => {
 
   const appUrl = {
-    dev: "com.suez.tsme",
-    devAndroid: "intent://#Intent;scheme=https;package=com.suez.tsme;end",
+    dev: "com.suez.tsme://",
+    devAndroid: "intent://resplendent-boba-1e1c62.netlify.app#Intent;scheme=https;package=com.suez.tsme;end",
     staging: "...",
     prod: "com./myapp://reset-password?user=test&token=13245",
     fallbackIosURL: 'https://toutsurmoneau.fr',
@@ -15,21 +15,21 @@ const LambdaDemo = () => {
  }
  
  
- useEffect(() => {
+//  useEffect(() => {
  
-    if (isAndroid || isIOS) {
-        // Try to open the native app when the page is loaded
-        window.location.replace(appUrl.dev);
-        // Show popup confirmation
-    }
- // eslint-disable-next-line react-hooks/exhaustive-deps
- }, []);
+//     if (isAndroid || isIOS) {
+//         // Try to open the native app when the page is loaded
+//         window.location.replace(appUrl.dev);
+//         // Show popup confirmation
+//     }
+//  // eslint-disable-next-line react-hooks/exhaustive-deps
+//  }, []);
  
  // User confirms he want to open the app popup
  function handleAppRedirect() {
  
     // Try to open the when user click in the "confirm" in popup
-    window.location.replace(appUrl.dev);
+    window.location.replace(appUrl.devAndroid);
     if (isAndroid) {
         setTimeout(() => {
             // If the app is not installed,
