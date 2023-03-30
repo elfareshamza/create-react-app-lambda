e6286c2f4b675aa96a9fef9c7876c235c2935647
El Fares Hamza <hamza.elfares.ext@suez.com>
2023-03-30 00:52:30 +0000

    test first code with handle click

--- src/App.js
+++ src/App.js
@@ -1,35 +1,60 @@
-import React, { Component } from "react"
+import React, { Component, useEffect } from "react"
+import { isAndroid, isIOS } from 'react-device-detect';
 import logo from "./logo.svg"
 import "./App.css"
 
-class LambdaDemo extends Component {
-  constructor(props) {
-    super(props)
-    this.state = { loading: false, msg: null }
-  }
+const LambdaDemo = () => {
 
-  handleClick = api => e => {
-    e.preventDefault()
+  const appUrl = {
+    dev: "com.suez.tsme",
+    staging: "...",
+    prod: "com./myapp://reset-password?user=test&token=13245",
+    fallbackIosURL: 'https://toutsurmoneau.fr',
+    fallbackAndroidURL: 'https://toutsurmoneau.fr',
+ }
+ 
+ 
+ useEffect(() => {
+ 
+    if (isAndroid || isIOS) {
+        // Try to open the native app when the page is loaded
+        window.location.replace(appUrl.dev);
+        // Show popup confirmation
+    }
+ // eslint-disable-next-line react-hooks/exhaustive-deps
+ }, []);
+ 
+ // User confirms he want to open the app popup
+ function handleAppRedirect() {
+ 
+    // Try to open the when user click in the "confirm" in popup
+    window.location.replace(appUrl.dev);
+    if (isAndroid) {
+        setTimeout(() => {
+            // If the app is not installed,
+            // then the user is redirect to the Play Store
+            window.location.replace(appUrl.fallbackAndroidURL);
+        }, 3000);
+    } else if (isIOS) {
+        setTimeout(() => {
+            // If the app is not installed
+            // then the user is reedirect to the App Store
+            window.location.replace(appUrl.fallbackIosURL);
+        }, 3000);
+    }
+ }
 
-    this.setState({ loading: true })
-    fetch("/.netlify/functions/" + api)
-      .then(response => response.json())
-      .then(json => this.setState({ loading: false, msg: json.msg }))
-  }
 
-  render() {
-    const { loading, msg } = this.state
 
     return (
       <p>
-        <button onClick={this.handleClick("hello")}>{loading ? "Loading..." : "Call Lambda"}</button>
-        <button onClick={this.handleClick("async-dadjoke")}>{loading ? "Loading..." : "Call Async Lambda"}</button>
-        <br />
-        <span>{msg}</span>
+       hey hello.
+
+       <div onClick={() => handleAppRedirect()} >click to handle redirect</div>
       </p>
     )
-  }
-}
+
+};
 
 class App extends Component {
   render() {
